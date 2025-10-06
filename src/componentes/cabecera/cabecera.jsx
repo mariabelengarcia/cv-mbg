daba3d8af6e8ecf00de426d941deb6399576b457
Belen Garcia <mariabelengarcia.dcv@gmail.com>
2025-10-06 19:59:57 -0300

    :sparkles:agrega funcionalidad de aumentar edad y de eliminar filas

--- src/componentes/cabecera/cabecera.jsx
+++ src/componentes/cabecera/cabecera.jsx
@@ -1,14 +1,24 @@
+import {useState} from "react";
 import Panel from "../panel/panel";
+import Boton from "../boton/boton";
 import "./cabecera.css"
 
 
 export default function Cabecera() {
+
+  const [edad, setEdad] = useState(32); // estado inicial
+
+  const aumentarEdad = () => {
+    setEdad(edad + 1);
+  };
   return (
     <Panel className="cabecera">
       <div>
         <h1>María Belén García</h1>
         <h2>Diseñadora en Comunicación Visual</h2>
-        <h3>32 años | La Plata | Buenos Aires</h3>
+        <h3>{edad} años | La Plata | Buenos Aires</h3>
+           <Boton variant="primario" texto="Aumentar edad" onClick={aumentarEdad} ></Boton>
+        
       </div>
     </Panel>
   );
--- src/componentes/experiencia/experiencia.jsx
+++ src/componentes/experiencia/experiencia.jsx
@@ -5,35 +5,49 @@ import Individual from "./individual";
 import {getExperiencies} from "./listaExperiencias";
 import "./experiencia.css";
 
-
 export default function Experiencia(){
-    const [listaExperiencias] =
+    const [listaExperiencias, setExperiencias] =
         useState(getExperiencies());
+
+
+
+  // 🗑️ Eliminar última
+  const eliminarUltima = () => {
+    setExperiencias(prev => prev.slice(0, -1));
+  };
+
+  // 🗑️ Eliminar primera
+  const eliminarPrimera = () => {
+    setExperiencias(prev => prev.slice(1));
+  };
+
   return (
     <Panel>
       <div className="experiencia">
         <h2>Experiencia laboral</h2>
         <div class="d-flex">
-          <Boton variant="secundario" texto="Eliminar última"></Boton>
-          <Boton variant="secundario" texto="Eliminar primera"></Boton>
-          <Boton variant="primario" texto="Agregar"></Boton>
+          <Boton variant="secundario" texto="Eliminar última" onClick={eliminarUltima}></Boton>
+          <Boton variant="secundario" texto="Eliminar primera" onClick={eliminarPrimera}></Boton>
+       
         </div>
+        
       </div>
 
       <table>
         <thead>
           <tr>
-            <th>Empresa</th>
             <th>Puesto</th>
+            <th>Institución / Empresa</th>
             <th>Periodo</th>
           </tr>
         </thead>
         <tbody>
-          {listaExperiencias.map((exp, index) => (
-            <Individual key={index} experience={exp} />
+          {listaExperiencias.map((exp) => (
+            <Individual key={exp.id} experience={exp} />
           ))}
         </tbody>
       </table>
     </Panel>
   );
 }
+
